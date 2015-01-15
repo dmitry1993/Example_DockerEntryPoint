d9945a0d734e9437cf0dd57d3a434059f7595c2b
Dmitry Kvochkin <dmitrykvochkin@gmail.com>
2015-01-15 18:17:41 +0000

    save

--- pullrepo.sh
+++ pullrepo.sh
@@ -4,13 +4,20 @@ set -e
 
 cd home
 git init
-#TEMP=`git clone https://$@@github.com/AntonioJFSousa/Trading.git`
+TEMP=`git clone https://$@@github.com/AntonioJFSousa/Trading.git`
 
-git clone https://$@@github.com/AntonioJFSousa/Trading.git
+#git clone https://$@@github.com/AntonioJFSousa/Trading.git
+
+
 
 #echo $TEMP
 
-#exec $TEMP
+exec $TEMP
+
+cd Trading/utrade
+
+node utrade.js
+
 
 #echo "ola"
 #top
